33f33fe9e97f2b25da47c9e95503acf93aed7f38
Abhi samadhiya <abhisamadhiya@Abhis-MacBook-Air.local>
2025-04-04 12:09:27 +0530

    Fixed CORS and DB connection

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -9,11 +9,16 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "*",  
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+
+app.use(
+  cors({
+    origin: ["https://campaign-manager-nine.vercel.app"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
@@ -26,6 +31,5 @@ app.use("/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  connectDB();
   console.log(`Server running on port ${PORT}`);
 });
